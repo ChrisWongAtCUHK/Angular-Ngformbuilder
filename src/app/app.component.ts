3af6129c30070051c2267d11418f2e439cebb4c8
chriswongatcuhk <wc38.wong@gmail.com>
2019-05-30 19:40:07 +0800

    Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, NgZone } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-ngformbuilder';
+export class AppComponent implements AfterViewInit {
+  title = 'Angular NgFormbuilder';
+
+  constructor(private ngZone: NgZone) {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      // delay 2 seconds waiting scripts to load
+      this.ngZone.runOutsideAngular(() => {
+        $(document.getElementById('fb-editor')).formBuilder();
+      });
+
+    }, 2000)
+  }
 }
